7b33a03b22319f75829ff3b1358830c2e6e70e61
liuliyi <liu.liyi.l@cn.ricoh.com>
2021-08-24 17:49:54 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: "Sapphire611",
+  description: "Description ... ",
+  themeConfig: {
+    displayAllHeaders: true, // 默认值：false
+    sidebar: ["/"],
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Node', link: '/node/' },
+    ],
+    sidebar: [
+      '/',
+      '/guide/',
+      '/node/',
+    ]
+  },
+};
