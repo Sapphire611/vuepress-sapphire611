a67d189c69d0eca4ca08b8272837eef83a456285
Liu Liyi <liyi.liu@carota.ai>
2023-10-27 17:50:01 +0800

    win 脚本

---
+++ scripts/deploy-github.bat
@@ -0,0 +1,15 @@
+@echo off
+
+echo git Init...
+git init
+
+echo git add ...
+git add .
+
+echo git commit...
+git commit -m "deploy"
+
+echo git push...
+git push -f git@github.com:sapphire611/sapphire611.github.io.git master
+
+echo Done
