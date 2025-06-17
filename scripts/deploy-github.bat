37e45d2047de29870d818415261f8a0df3466a08
Sapphire611 <liuliyi611@gmail.com>
2025-06-17 21:47:49 +0800

    🐞 fix: 修复编译脚本

--- scripts/deploy-github.bat
+++ scripts/deploy-github.bat
@@ -5,13 +5,16 @@ cd ./docs/.vuepress/dist
 echo git Init...
 git init
 
+git checkout -b master
+
 echo git add ...
-git add .
+git add -A
 
 echo git commit...
 git commit -m "deploy"
 
 echo git push...
-git push -f git@github.com:sapphire611/sapphire611.github.io.git master
+git remote add origin git@github.com:Sapphire611/sapphire611.github.io.git
+git push --set-upstream origin master --force
 
 echo Done
--- scripts/deploy-github.sh
+++ scripts/deploy-github.sh
@@ -1,15 +1,8 @@
 npm run build
 cd docs/.vuepress/dist
-
 git init
+git checkout -b master
 git add -A
 git commit -m 'deploy'
-
-# Check current branch name and push appropriately
-branch_name=$(git branch --show-current)
-if [ -z "$branch_name" ]; then
-  # Handle detached HEAD state (first commit creates branch)
-  git checkout -b master  # Force create master branch
-fi
-
-git push -f git@github.com:sapphire611/sapphire611.github.io.git ${branch_name:-master}:master
+git remote add origin git@github.com:Sapphire611/sapphire611.github.io.git
+git push --set-upstream origin master --force
